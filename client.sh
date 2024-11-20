6d3bb9f0cdb188a919103d4ee9d3fc367bbad794
ferranbone <ferran.bone@enti.cat>
2024-11-20 11:25:22 +0100

    avances

--- client.sh
+++ client.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+IP_SERVER=$1
+IP=`ip a | grep "scope global" | xargs | cut -d " " -f 2 | cut -d "/" -f 1`
 PORT="2022"
 echo "Cliente de Dragon Magia Abuelita Miedo 2022"
 echo "1.ENVIO DE CABECERA"
-echo "DMAM" | nc 127.0.0.1 $PORT
+echo "DMAM $IP" | nc $IP_SERVER $PORT
 DATA=`nc -l $PORT`
 echo "3.COMPROBANDO"
 if [ "$DATA" != "OK_HEADER" ]
@@ -13,7 +15,7 @@ fi
 echo "4. COMPROBANDO HEADER"
 read FILE_NAME
 echo "FILE_NAME" > nombre.txt
-echo "FILE_NAME $FILE_NAME" | nc 127.0.0.1 $PORT
+echo "FILE_NAME $FILE_NAME" | nc $IP_SERVER $PORT
 echo "7. RECIBIENDO"
 DATA=`nc -l $PORT`
 
@@ -25,4 +27,4 @@ fi
 
 echo "8. ENVIANDO CONTENIDO"
 
-cat client/$FILE_NAME | nc localhost $PORT
+cat client/$FILE_NAME | nc $IP_SERVER $PORT
--- server.sh
+++ server.sh
@@ -3,26 +3,28 @@ PORT="2022"
 echo "Servidor de Dragon Magia Abuelita Miedo 2022"
 echo "0. ESCUCHAMOS"
 DATA=`nc -l $PORT`
-if [ "$DATA" != "DMAM" ]
+HEADER=`echo $DATA | cut -d " " -f 1`
+IP=`echo $DATA | cut -d " " -f 2`
+if [ "$HEADER" != "DMAM" ]
 then
     echo "ERROR 1: Cabecera incorrecta"
-    echo "KO_HEADER" | nc localhost 2022
+    echo "KO_HEADER" | nc $IP 2022
     exit 1
 fi
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost 2022
+echo "OK_HEADER" | nc $IP 2022
 FILE_DATA=$(nc -l $PORT)
 PREFIJO=$(echo $FILE_DATA | cut -d " " -f 1)
 echo "5. COMPROBANDO"
 if [ "$PREFIJO" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_FILE_NAME" | nc localhost $PORT
+	echo "KO_FILE_NAME" | nc $IP $PORT
 	exit 2
 fi 
 
 echo "6. ENVIANDO OK_FILE_NAME"
-echo "OK_FILE_NAME" | nc localhost $PORT
+echo "OK_FILE_NAME" | nc $IP $PORT
 
 DATA=`nc -l $PORT`
 
